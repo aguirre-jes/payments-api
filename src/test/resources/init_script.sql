c8b8924b93bdf18dabf7ce47db399003aab389ce
Jesús Aguirre <66501518+aguirre-jes@users.noreply.github.com>
2025-03-24 23:02:19 +0000

    Update PaymentRepositoryImplTest and init_script.sql for payment status and ID adjustments

--- src/test/resources/init_script.sql
+++ src/test/resources/init_script.sql
@@ -20,10 +20,10 @@ VALUES (2, 250.50, 'EUR', SYSDATE, 'Pending', 102, 202, 'Bank Transfer', 'TXN123
 INSERT INTO PAYMENTS (id, amount, currency, payment_date, status, payer_id, payee_id, payment_method, transaction_id, description) 
 VALUES (3, 75.75, 'GBP', SYSDATE, 'Failed', 103, 203, 'PayPal', 'TXN123458', 'Payment for invoice #125');
 INSERT INTO PAYMENTS (id, amount, currency, payment_date, status, payer_id, payee_id, payment_method, transaction_id, description) 
-VALUES (4, 75.75, 'GBP', SYSDATE, 'Failed', 103, 203, 'PayPal', 'TXN123459', 'Payment for invoice #124');
+VALUES (4, 75.75, 'GBP', SYSDATE, 'Completed', 103, 203, 'PayPal', 'TXN123459', 'Payment for invoice #124');
 INSERT INTO PAYMENTS (id, amount, currency, payment_date, status, payer_id, payee_id, payment_method, transaction_id, description) 
-VALUES (5, 75.75, 'GBP', SYSDATE, 'Failed', 103, 203, 'PayPal', 'TXN123460', 'Payment for invoice #125');
+VALUES (5, 75.75, 'GBP', SYSDATE, 'Completed', 103, 203, 'PayPal', 'TXN123460', 'Payment for invoice #125');
 INSERT INTO PAYMENTS (id, amount, currency, payment_date, status, payer_id, payee_id, payment_method, transaction_id, description) 
-VALUES (6, 75.75, 'GBP', SYSDATE, 'Failed', 103, 203, 'PayPal', 'TXN123461', 'Payment for invoice #126');
+VALUES (6, 75.75, 'GBP', SYSDATE, 'Completed', 103, 203, 'PayPal', 'TXN123461', 'Payment for invoice #126');
 INSERT INTO PAYMENTS (id, amount, currency, payment_date, status, payer_id, payee_id, payment_method, transaction_id, description) 
-VALUES (7, 75.75, 'GBP', SYSDATE, 'Failed', 103, 203, 'PayPal', 'TXN123462', 'Payment for invoice #127');
+VALUES (7, 75.75, 'GBP', SYSDATE, 'Pending', 103, 203, 'PayPal', 'TXN123462', 'Payment for invoice #127');
