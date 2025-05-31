7eeb5876ced79a0a0ebe7ad09f8b197bbfa64d0d
aguirre-jes <66501518+aguirre-jes@users.noreply.github.com>
2025-05-30 20:29:04 -0500

    Refactor PaymentResponse class to improve code formatting and readability

--- src/main/java/jeaguirre/me/adapters/inputs/rest/dto/PaymentResponse.java
+++ src/main/java/jeaguirre/me/adapters/inputs/rest/dto/PaymentResponse.java
@@ -1,13 +1,14 @@
 package jeaguirre.me.adapters.inputs.rest.dto;
 
-
 public class PaymentResponse extends PaymentDetails {
     private int id;
-
+    
+    @Override
     public int getId() {
         return id;
     }
-
+    
+    @Override
     public void setId(int id) {
         this.id = id;
     }
